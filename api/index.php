73289934a0f0433621fc24c11b61ab1842b174dd
domkh@12 <dommr86@gmail.com>
2025-08-08 02:51:41 +0700

    update

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
+// Ensure we're in the right directory
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
 
-require __DIR__ . "/../public/index.php";
+// Change to Laravel root
+chdir(dirname(__DIR__));
+
+// Load Laravel
+require_once __DIR__ . '/../public/index.php';
